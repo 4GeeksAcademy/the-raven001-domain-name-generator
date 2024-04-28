ba0430d7ec560138ed4ad693058eaf4e35c15e54
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-04-28 19:51:00 +0000

    First commit of Domain name generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  for (let i in pronoun) {
+    for (let x in adj) {
+      for (let z in noun) {
+        for (let p in ext) {
+          console.log(pronoun[i] + adj[x] + noun[z] + ext[p]);
+        }
+      }
+    }
+  }
 };
